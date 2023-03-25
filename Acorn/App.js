d138b16df90b9069333bade628aa95de8b038f21
charlie <you@example.com>
2023-03-25 10:03:38 -0700

    added login page

--- Acorn/App.js
+++ Acorn/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { View, StyleSheet } from "react-native";
 import WelcomeScreen from './app/Screens/WelcomeScreen';
+import LoginScreen from './app/Screens/LoginScreen';
 
 export default function App() {
   return (
-      <WelcomeScreen />
+      // <WelcomeScreen />
+      <LoginScreen />
   )
+
 }
