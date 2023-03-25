909808d318bcc5fd4540400b3edfefccf0f9fb0e
Miranda-Tang <mirandatang0708@gmail.com>
2023-03-25 14:36:44 -0700

    HomeCalendarScreen finished

--- Acorn/app/Screens/HomeCalendarScreen.js
+++ Acorn/app/Screens/HomeCalendarScreen.js
@@ -1,15 +1,24 @@
 import React, { useState } from "react";
-import { SafeAreaView, ScrollView, StyleSheet, View } from "react-native";
-import { Calendar } from "react-native-calendars";
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  View,
+  Modal,
+  TouchableOpacity,
+  Image,
+  Text,
+} from "react-native";
+import { Calendar, CalendarList } from "react-native-calendars";
+import upset from "../assets/upset.png";
+import stress from "../assets/stress.png";
+import sleepy from "../assets/sleepy.png";
+import happy from "../assets/happy.png";
+import cry from "../assets/cry.png";
 
 function HomeCalendarScreen() {
-  //   const testBackend = () => {
-  //     fetch("http://127.0.0.1:8000/mood/")
-  //       .then((res) => res.json())
-  //       .then((result) => {
-  //         console.log(result);
-  //       });
-  //   };
+  const [modalVisible, setModalVisible] = useState(false);
+  const [rowGap, setRowGap] = useState(10);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -43,6 +52,7 @@ function HomeCalendarScreen() {
         }}
         onDayPress={(date) => {
           console.log("Selected date: ", date.dateString);
+          setModalVisible(true);
         }}
         onMonthChange={(date) => console.log("onMonthChange", date)}
         onPressArrowLeft={(goToPreviousMonth) => {
@@ -58,6 +68,113 @@ function HomeCalendarScreen() {
         maxDate={"2023-12-31"}
         hideExtraDays={true}
       />
+      <Modal
+        transparent={true}
+        visible={modalVisible}
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          alignSelf: "center",
+          marginTop: "10%",
+          marginBottom: "10%",
+        }}
+      >
+        <View
+          style={{
+            backgroundColor: "#EEE4FF",
+            width: "45%",
+            height: "50%",
+            alignItems: "center",
+            justifyContent: "center",
+            alignSelf: "center",
+            marginTop: "10%",
+            marginBottom: "10%",
+            borderRadius: "5%",
+          }}
+        >
+          <View
+            style={{
+              backgroundColor: "#ffffff",
+              margin: 30,
+              padding: 30,
+              borderRadius: 10,
+              flex: 1,
+              width: "90%",
+              height: "90%",
+              alignItems: "center",
+              justifyContent: "center",
+              alignSelf: "center",
+            }}
+          >
+            <Text style={styles.text}>Log Mood</Text>
+            <View
+              style={{
+                backgroundColor: "#ffffff",
+                margin: 30,
+                padding: 30,
+                borderRadius: 10,
+                flex: 1,
+                // width: "100%",
+                // height: "100%",
+                alignItems: "stretch",
+                justifyContent: "center",
+                alignSelf: "center",
+                flexDirection: "row",
+              }}
+            >
+              <TouchableOpacity
+                activeOpacity={0.5}
+                onPress={() => {
+                  console.log("upset");
+                }}
+              >
+                <Image source={upset} style={styles.emoji}></Image>
+              </TouchableOpacity>
+              <TouchableOpacity
+                activeOpacity={0.5}
+                onPress={() => {
+                  console.log("stress");
+                }}
+              >
+                <Image source={stress} style={styles.emoji}></Image>
+              </TouchableOpacity>
+              <TouchableOpacity
+                activeOpacity={0.5}
+                onPress={() => {
+                  console.log("sleepy");
+                }}
+              >
+                <Image source={sleepy} style={styles.emoji}></Image>
+              </TouchableOpacity>
+              <TouchableOpacity
+                activeOpacity={0.5}
+                onPress={() => {
+                  console.log("happy");
+                }}
+              >
+                <Image source={happy} style={styles.emoji}></Image>
+              </TouchableOpacity>
+              <TouchableOpacity
+                activeOpacity={0.5}
+                onPress={() => {
+                  console.log("cry");
+                }}
+              >
+                <Image source={cry} style={styles.emoji}></Image>
+              </TouchableOpacity>
+            </View>
+
+            <TouchableOpacity
+              onPress={() => {
+                setModalVisible(false);
+              }}
+              style={styles.submitButon}
+            >
+              <Text style={styles.submitButtonText}>Submit</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </SafeAreaView>
   );
 }
@@ -71,6 +188,37 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     elevation: 4,
     margin: 40,
+  },
+  text: {
+    color: "#655DBB",
+    fontWeight: "bold",
+    fontSize: 27,
+    textAlign: "center",
+  },
+  note_text: {
+    color: "#655DBB",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  emoji: {
+    height: 80,
+    width: 80,
+    margin: 10,
+  },
+  submitButon: {
+    marginTop: 20,
+    width: 150,
+    height: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+    borderRadius: 40,
+    backgroundColor: "#655DBB",
+  },
+  submitButtonText: {
+    fontWeight: "bold",
+    fontSize: 16,
+    color: "#EEE4FF",
   },
 });
 
